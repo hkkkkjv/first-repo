ccdaf44dd58f9dbafffb191c5c2dd59b1e8994b1
hkkkkjv <hkkkkjv@mail.ru>
2023-09-08 20:12:32 +0300

    Добавила грустную скобку к выводу

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub! I did it! My brain is broken ");
+        System.out.println("Hello, GitHub! I did it! My brain is broken(((( ");
     }
 }
